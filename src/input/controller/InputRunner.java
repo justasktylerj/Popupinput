e0b0f96adeaae6d23e610a90297cfe26a21046f6
Tyler jarrard <tjar2074@740s-pl-104.local>
2015-09-24 08:28:39 -0600

    started

---
+++ src/input/controller/InputRunner.java
@@ -0,0 +1,10 @@
+package input.controller;
+
+public class InputRunner 
+{
+     public static void main (String [] args)
+     {
+    	 PopupController myInput = new PopupController();
+    	 myInput.start();
+     }
+}
